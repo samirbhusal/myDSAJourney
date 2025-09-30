4a70347736d91ad29223e4b9e426b0e52f00ceff
samir bhusal <bhusalsamir907@gmail.com>
2025-09-29 22:32:59 -0500

    new changes and checkLeader problem

---
+++ arrays/ArraysProblems.py
@@ -0,0 +1,35 @@
+#1 Pair of numbers (int) that sum to target
+def check_for_target(nums, target):
+    left = 0
+    right = len(nums)-1
+    while left < right:
+        current_Sum = nums[left] + nums[right]
+        if current_Sum == target:
+            return True
+
+        if current_Sum > target:
+            right -= 1
+        else:
+            left += 1
+    return False
+
+#Leaders in an array
+# Given an array arr[] of size n, the task is to find all the Leaders in the array.
+# An element is a Leader if it is greater than or equal to all the elements to its right side.
+
+def checkLeaderInArray(nums):
+    leaders = []
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] < nums[j]:
+                break
+        else:
+            leaders.append(nums[i])
+
+    return leaders
+
+print("arr: ", checkLeaderInArray([1,2,3,4,5]))
+print("arr: ", checkLeaderInArray([1,2,3,5,4]))
+
---
+++ arrays/twoSum.py
@@ -0,0 +1,8 @@
+# from typing import List
+#
+# class TwoSumSolution:
+#     def twoSum(self, nums: List[int], target) -> List[int]:
+#         seen = set()
+#         for i, num in enumerate(nums):
+#
+# 
